fbaa5faddc5943519e0019114110dc19b9506a69
Logan Hallee <72926928+lhallee@users.noreply.github.com>
2022-12-16 09:34:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -66,25 +66,25 @@ def main(config):
 def run_from_main():
     parser = argparse.ArgumentParser()
     # model hyper-parameters
-    parser.add_argument('--image_size', type=int, default=512)
+    parser.add_argument('--image_size', type=int, default=1024)
     parser.add_argument('--t', type=int, default=3, help='t for Recurrent step of R2U_Net or R2AttU_Net')
     parser.add_argument('--num_class', type=int, default=2, help='Number of classes for segmentation')
 
     # training hyper-parameters
     parser.add_argument('--img_ch', type=int, default=3)
     parser.add_argument('--num_epochs', type=int, default=75)
-    parser.add_argument('--batch_size', type=int, default=4)
+    parser.add_argument('--batch_size', type=int, default=1)
     parser.add_argument('--lr', type=float, default=0.003)
     parser.add_argument('--beta1', type=float, default=0.5)  # momentum1 in Adam
     parser.add_argument('--beta2', type=float, default=0.999)  # momentum2 in Adam
     parser.add_argument('--scheduler', type=str, default='cosine', help='None, exp, cosine')
-    parser.add_argument('--loss', type=str, default='DiceBCE', help='BCE, DiceBCE, IOU, CE')
+    parser.add_argument('--loss', type=str, default='IOU', help='BCE, DiceBCE, IOU, CE')
 
     # misc
     parser.add_argument('--mode', type=str, default='train')
     parser.add_argument('--model_type', type=str, default='R2AttU_Net', help='U_Net/R2U_Net/AttU_Net/R2AttU_Net')
     parser.add_argument('--result_path', type=str, default='./result/')
-    parser.add_argument('--model_path', type=str, default='/content/drive/MyDrive/Logan/CV/saved weights/pretrainedBOTH128.pkl')
+    parser.add_argument('--model_path', type=str, default='/content/drive/MyDrive/Logan/CV/saved weights/pretrainedBOTH512.pkl')
     parser.add_argument('--img_path', type=str, default='./img/')
     parser.add_argument('--GT_path', type=str, default='./GT/')
     parser.add_argument('--eval_img_path', type=str, default='./eval_img/')
